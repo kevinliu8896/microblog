98d170ab2182b8c5b3bed10cb3aa5dc53de5a48d
Mehverlous <davidackuaku3@gmail.com>
2023-03-11 13:23:01 -0500

    Implementing email authentication

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,11 +1,11 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, session, flash, request
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_babel import _
 from app import db
 from app.auth import bp
 from app.auth.forms import LoginForm, RegistrationForm, \
-    ResetPasswordRequestForm, ResetPasswordForm
+    ResetPasswordRequestForm, ResetPasswordForm, LoginAuthentication
 from app.models import User
 from app.auth.email import send_password_reset_email
 
@@ -20,13 +20,32 @@ def login():
         if user is None or not user.check_password(form.password.data):
             flash(_('Invalid username or password'))
             return redirect(url_for('auth.login'))
+        code = "1234"
+        session["code"] = code 
+        session["user"] = user
+        return redirect(url_for('auth.login_authentication'))
+    return render_template('auth/login.html', title=_('Sign In'), form=form)
+
+@bp.route('/login_authentication', methods=['GET', 'POST'])
+def login_authentication():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = LoginAuthentication()
+    if form.validate_on_submit():
+        code = request.form.get('verificationCode')
+        message = "Current Verification Code: " + str(code)
+        flash(message)
+        if session["code"] != code:
+            flash(_('Invalid username or password'))
+            return redirect(url_for('auth.login_authentication'))
+        user = session["user"]
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('main.index')
-        return redirect(next_page)
-    return render_template('auth/login.html', title=_('Sign In'), form=form)
-
+        return redirect(next_page)    
+    return render_template('auth/login_authentication.html', title=_('Verify'),
+                           form=form)
 
 @bp.route('/logout')
 def logout():
