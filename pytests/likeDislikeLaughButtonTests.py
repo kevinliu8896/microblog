ed56a7907f0c8fcbea3d350cf488636df0e93476
JoshuaPicchioni <123222957+JoshuaPicchioni@users.noreply.github.com>
2023-03-12 14:10:42 -0400

    Modifed Tests

--- pytests/likeDislikeLaughButtonTests.py
+++ pytests/likeDislikeLaughButtonTests.py
@@ -29,30 +29,18 @@ class TestLikeDislikeLaughButtonTests():
   def teardown_method(self, method):
     self.driver.quit()
   
-  def test_1CreateUserTest(self):
-
+  
+  def test_createUserTest(self):
     self.driver.get("http://127.0.0.1:5000/auth/login?next=%2F")
-
-    self.driver.set_window_size(1281, 1097)
+    self.driver.set_window_size(1205, 1077)
     self.driver.find_element(By.ID, "username").send_keys("admin-user")
     self.driver.find_element(By.ID, "password").send_keys("ABC123")
     self.driver.find_element(By.LINK_TEXT, "Click to Register!").click()
     self.driver.find_element(By.ID, "username").click()
-    self.driver.find_element(By.ID, "username").send_keys("WebDriver")
+    self.driver.find_element(By.ID, "username").send_keys("Joshua")
     self.driver.find_element(By.ID, "email").click()
-    self.driver.find_element(By.ID, "email").send_keys("dummyjenkinsemail@gmail.com")
+    self.driver.find_element(By.ID, "email").send_keys("themanjamesgreen@gmail.com")
     self.driver.find_element(By.ID, "password").click()
-    self.driver.find_element(By.ID, "password").send_keys("Test1238")
-    self.driver.find_element(By.ID, "password2").click()
-    self.driver.find_element(By.ID, "password2").send_keys("Test1238")
-    self.driver.find_element(By.ID, "submit").click()
-    self.driver.find_element(By.LINK_TEXT, "Click to Register!").click()
-    self.driver.find_element(By.ID, "username").click()
-    self.driver.find_element(By.ID, "username").send_keys("WebDriver")
-    self.driver.find_element(By.ID, "username").click()
-    self.driver.find_element(By.ID, "username").send_keys("SeleniumWebDriver")
-    self.driver.find_element(By.ID, "email").click()
-    self.driver.find_element(By.ID, "email").send_keys("TestEmail@gmail.com")
     self.driver.find_element(By.ID, "password").click()
     self.driver.find_element(By.ID, "password").send_keys("Test1238")
     self.driver.find_element(By.ID, "password2").click()
@@ -60,81 +48,11 @@ class TestLikeDislikeLaughButtonTests():
     self.driver.find_element(By.ID, "submit").click()
     self.driver.find_element(By.ID, "username").send_keys("admin-user")
     self.driver.find_element(By.ID, "password").send_keys("ABC123")
+    self.driver.find_element(By.ID, "username").click()
+    self.driver.find_element(By.ID, "username").click()
+    self.driver.find_element(By.ID, "username").send_keys("Joshua")
     self.driver.find_element(By.CSS_SELECTOR, "html").click()
-    self.driver.find_element(By.ID, "username").send_keys("SeleniumWebDriver")
-    self.driver.find_element(By.CSS_SELECTOR, "body").click()
     self.driver.find_element(By.ID, "password").send_keys("Test1238")
-    self.driver.find_element(By.ID, "remember_me").click()
+    self.driver.find_element(By.CSS_SELECTOR, ".checkbox > label").click()
     self.driver.find_element(By.ID, "submit").click()
-  
-  def test_2MakePostTest(self):
-
-
-    self.driver.get("http://127.0.0.1:5000/")
-
-    self.driver.set_window_size(1281, 1097)
-    self.driver.find_element(By.ID, "post").click()
-    self.driver.find_element(By.ID, "post").send_keys("Hello from Selenium Webdriver!")
-    self.driver.find_element(By.ID, "submit").click()
-  
-  def test_3LikeDislikeTest(self):
-
-
-    self.driver.get("http://127.0.0.1:5000/")
-
-    self.driver.set_window_size(1281, 1097)
-    self.driver.find_element(By.ID, "like-count-55").click()
-    self.driver.find_element(By.ID, "dislike-btn-55").click()
-    self.driver.find_element(By.ID, "like-btn-55").click()
-    self.driver.find_element(By.ID, "dislike-btn-55").click()
-    self.driver.find_element(By.ID, "like-btn-55").click()
-    self.driver.find_element(By.ID, "dislike-btn-55").click()
-    self.driver.find_element(By.ID, "dislike-btn-55").click()
-    element = self.driver.find_element(By.ID, "dislike-btn-55")
-    actions = ActionChains(self.driver)
-    actions.double_click(element).perform()
-    self.driver.find_element(By.ID, "dislike-btn-55").click()
-    self.driver.find_element(By.ID, "like-btn-55").click()
-    self.driver.find_element(By.ID, "like-btn-55").click()
-    element = self.driver.find_element(By.ID, "like-btn-55")
-    actions = ActionChains(self.driver)
-    actions.double_click(element).perform()
-    self.driver.find_element(By.ID, "like-btn-55").click()
-    self.driver.find_element(By.ID, "like-btn-55").click()
-    self.driver.find_element(By.ID, "like-btn-55").click()
-    self.driver.find_element(By.ID, "dislike-btn-55").click()
-    self.driver.find_element(By.ID, "dislike-btn-55").click()
-    element = self.driver.find_element(By.ID, "dislike-btn-55")
-    actions = ActionChains(self.driver)
-    actions.double_click(element).perform()
-    self.driver.find_element(By.ID, "dislike-btn-55").click()
-    self.driver.find_element(By.ID, "like-count-55").click()
-    self.driver.find_element(By.ID, "dislike-btn-55").click()
-    self.driver.find_element(By.ID, "like-count-55").click()
-    self.driver.find_element(By.ID, "dislike-btn-55").click()
-  
-  def test_4LaughTest(self):
-
-
-    self.driver.get("http://127.0.0.1:5000/")
-
-
-    self.driver.set_window_size(1281, 1097)
-    self.driver.find_element(By.ID, "laugh-btn-55").click()
-    self.driver.find_element(By.ID, "laugh-btn-55").click()
-    element = self.driver.find_element(By.ID, "laugh-btn-55")
-    actions = ActionChains(self.driver)
-    actions.double_click(element).perform()
-    self.driver.find_element(By.ID, "laugh-btn-55").click()
-    self.driver.find_element(By.ID, "laugh-btn-55").click()
-    self.driver.find_element(By.ID, "laugh-btn-55").click()
-    self.driver.find_element(By.ID, "laugh-btn-55").click()
-    self.driver.find_element(By.ID, "laugh-btn-55").click()
-    self.driver.find_element(By.ID, "laugh-btn-55").click()
-    self.driver.find_element(By.ID, "laugh-btn-55").click()
-    self.driver.find_element(By.ID, "laugh-btn-55").click()
-    self.driver.find_element(By.ID, "laugh-btn-55").click()
-    self.driver.find_element(By.ID, "laugh-btn-55").click()
-    self.driver.find_element(By.ID, "laugh-btn-55").click()
-    self.driver.find_element(By.ID, "laugh-btn-55").click()
   
