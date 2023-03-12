52a449c1efb420dcc73887d2dba8465ff378f772
JoshuaPicchioni <123222957+JoshuaPicchioni@users.noreply.github.com>
2023-03-11 23:56:27 -0500

    Test Commit  (#60)
    * Added Like, Dislike and Laugh buttons with front and backend functioality
    
    * Fixed likes on other pages not being saved
    
    * Added Gerkin user stories
    
    * Removed .DS_Store
    
    * Fixed messssage not working after buttons added
    
    * Modified Tests for Like, Dislike and Laugh
    
    * Added modifcations to the tests
    
    * Modified Selenium Driver Options
    
    * Modifed selenium driver options

--- pytests/likeDislikeLaughButtonTests.py
+++ pytests/likeDislikeLaughButtonTests.py
@@ -15,7 +15,14 @@ class TestLikeDislikeLaughButtonTests():
 
   def setup_method(self, method):
     options = Options()
-    options.add_argument('--headless')
+    options.add_argument("--headless")
+    options.add_argument("window-size=1400,1500")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("start-maximized")
+    options.add_argument("enable-automation")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-dev-shm-usage")
     self.driver = webdriver.Chrome(options=options)
     self.vars = {}
   
